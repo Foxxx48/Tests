c33de60004c6163c96ea74fca6e66cd2260dd520
Foxxx48 <Foxxdim@mail.ru>
2023-01-13 17:22:39 +0300

    added functions to ResourceManagerTest

--- app/src/test/java/com/example/tests/ResourceManagerTest.kt
+++ app/src/test/java/com/example/tests/ResourceManagerTest.kt
@@ -1,5 +1,6 @@
 package com.example.tests
 
+import org.junit.Assert.assertEquals
 import org.junit.Test
 
 class ResourceManagerTest {
@@ -7,11 +8,101 @@ class ResourceManagerTest {
     @Test
     fun consumeResourceAfterSetResourceCallReceivesResource() {
         //arrange
-            val resourceManager = ResourceManager<String>(
-                TestExecutor(), TestErrorHandler()
-            )
+        val resourceManager = createResourceManager()
+
+        val consumer = TestConsumer()
+
         //act
+        resourceManager.setResource("TEST")
+        resourceManager.consumeResource(consumer)
 
         //assert
+        assertEquals("TEST", consumer.lastResource)
+        assertEquals(1, consumer.invokeCount)
     }
+
+
+    @Test
+    fun consumeResourceCallsAfterSetResourceCallReceivesResourceEachConsumer() {
+        //arrange
+        val resourceManager = createResourceManager()
+
+        val consumer1 = TestConsumer()
+        val consumer2 = TestConsumer()
+
+        //act
+        resourceManager.setResource("TEST")
+        resourceManager.consumeResource(consumer1)
+        resourceManager.consumeResource(consumer2)
+
+
+        //assert
+        assertEquals("TEST", consumer1.lastResource)
+        assertEquals(1, consumer1.invokeCount)
+        assertEquals("TEST", consumer2.lastResource)
+        assertEquals(1, consumer2.invokeCount)
+    }
+
+    @Test
+    fun consumeResourceAfterSetResourceCallsReceiveLatestResource() {
+        val resourceManager = createResourceManager()
+
+        val consumer = TestConsumer()
+
+        resourceManager.setResource("TEST1")
+        resourceManager.setResource("TEST2")
+        resourceManager.consumeResource(consumer)
+
+        assertEquals("TEST2", consumer.lastResource)
+        assertEquals(1, consumer.invokeCount)
+    }
+
+    @Test
+    fun consumeResourceCallsWithSameConsumerCanReceiveTheSameResource() {
+        val resourceManager = createResourceManager()
+
+        val consumer = TestConsumer()
+
+        resourceManager.setResource("TEST")
+        resourceManager.consumeResource(consumer)
+        resourceManager.consumeResource(consumer)
+
+        assertEquals("TEST", consumer._resources[0])
+        assertEquals("TEST", consumer._resources[1])
+        assertEquals(2, consumer.invokeCount)
+    }
+
+    @Test
+    fun consumeResourceWithoutActiveResourceDoesNothing() {
+        val resourceManager = createResourceManager()
+
+        val consumer = TestConsumer()
+
+        assertEquals(0, consumer.invokeCount)
+    }
+
+    @Test
+    fun setResourceAfterConsumeResourceCallDeliversResourceToConsumer() {
+        val resourceManager = createResourceManager()
+
+        val consumer = TestConsumer()
+
+        resourceManager.consumeResource(consumer)
+        resourceManager.setResource("TEST")
+
+        assertEquals("TEST", consumer.lastResource)
+        assertEquals(1, consumer.invokeCount)
+    }
+
+    private fun createResourceManager(
+        testExecutor: TestExecutor = TestExecutor(),
+        testErrorHandler: TestErrorHandler = TestErrorHandler()
+    ): ResourceManager<String> {
+        return ResourceManager(
+            testExecutor, testErrorHandler
+        )
+
+    }
+
+
 }
